3a4eb5cbbf03b3e47fa963de5f5157503630b978
LAPTOP-347KIE4F\Michele <michele.marinelli77@gmail.com>
2018-08-20 10:11:51 +0200

    no message

---
+++ src/pages/Market.vue
@@ -0,0 +1,27 @@
+<template>
+  
+  <div class="playbuzz" data-id="1debab89-1b02-44a8-bc3f-f9f595a90679"></div>
+</template>
+
+<script>
+  //(function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = 'https://embed.playbuzz.com/sdk.js'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'playbuzz-sdk'));
+  export default {
+    created() {
+      //this.myFunction(document, 'script', 'playbuzz-sdk');
+    },
+    methods: {
+      
+      myFunction: function (d, s, id) {
+        alert('IN')
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById('playbuzz-sdk'))
+          return;
+        alert('IN1')
+        js = d.createElement(s);
+        js.id = 'playbuzz-sdk';
+        js.src = 'https://embed.playbuzz.com/sdk.js';
+        alert('IN2')
+      }
+    }
+  }
+</script>
